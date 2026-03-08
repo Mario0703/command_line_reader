6bc407fa8ad88c2ef80b5809cda15bc054077abd
Mario <mario@supermario.dk>
2026-03-08 10:52:12 +0100

    Add case-insensitive search function and corresponding tests

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,22 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> { //one can gi
     results
 }
 
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +39,19 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+        #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
