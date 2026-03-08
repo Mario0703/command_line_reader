307796dc3cf71daa862b75833bea99595a8d369b
Mario <mario@supermario.dk>
2026-03-08 10:50:49 +0100

    Implement search function with basic line matching and add initial tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
-pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+use std::result;
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> { //one can give a wilde card to arguments if not used, kinda usefull
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
